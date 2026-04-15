8a78a0396ceafbc4347e5e2c30eade3922c9fa53
osmarrdaniel <osmar.avalost@uanl.edu.mx>
2026-04-15 16:03:12 -0600

    Se creo init

---
+++ programa.py
@@ -0,0 +1,10 @@
+class Tenis:
+    def __init__(self, marca, modelo, precio, descuento, talla):
+        self.__marca = marca
+        self.__modelo = modelo 
+        self.__precio = precio 
+        self.__descuento = descuento 
+        self.__talla = talla 
+        self.__preciofinal = 0
+        
+    
