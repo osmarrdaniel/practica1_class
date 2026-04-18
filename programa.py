6e2a6bb81a364a5129e987139752def8f5b49175
osmarrdaniel <osmar.avalost@uanl.edu.mx>
2026-04-17 19:46:51 -0600

    Se creo get-set de modelo

--- programa.py
+++ programa.py
@@ -11,5 +11,8 @@ class Tenis:
         return self.__marca 
     def set_marca(self, marca):
         self.__marca = marca
+    def get_modelo(self):
+        return self.__modelo
+    def set_modelo(self, modelo):
+        self.__modelo = modelo 
         
-    
